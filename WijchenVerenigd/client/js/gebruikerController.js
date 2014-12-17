c59b61935e8d3fae45d585a11724b4286abfc142
Sanne Peters <sannepetersx@gmail.com>
2014-12-17 13:49:13 +0100

    added complete thing still have to perfectionize it

--- WijchenVerenigd/client/js/gebruikerController.js
+++ WijchenVerenigd/client/js/gebruikerController.js
@@ -20,4 +20,26 @@ var gebruikerController = function ($routeParams, $scope, $window, dbService) {
 			inlogMessage = res.message;
 		});
 	}
+
+	var gauge = new FlexGauge({
+		appendTo: '#example1',
+		animateEasing: true,
+
+		elementId: 'example2_canvas',
+		elementWidth: 170,
+		elementHeight: 170,
+
+		arcSize: 40,
+		arcAngleStart: 0,
+		arcFillPercent: .80,
+		arcStrokeFg: 5,
+		arcStrokeBg: 5,
+		animateSpeed: 0.5,
+		arcAngleStart: 0.85,
+		arcAngleEnd: 2.15,
+		dialValue: true,
+
+		styleArcFg: 'btn-success',
+		styleSrc: 'background-color'
+	});
 }
