656251a9cc6c39a19f44042fa4b8fea3dd311d96
serealKiller <developer@DWA-webdev-machine>
2014-12-17 17:08:53 +0100

    Revert "litte bit of an update for login and register but cant get gitEye to ignore the webstorm folders so they are uploading aswell :("
    This reverts commit 1cd69fcd4d275c6a0fcad3e2b59dac773f4acf8e.

--- WijchenVerenigd/client/js/gebruikerController.js
+++ WijchenVerenigd/client/js/gebruikerController.js
@@ -1,33 +1,23 @@
 var gebruikerController = function ($routeParams, $scope, $window, dbService) {
-    $scope.ingelogd = false;
-    $scope.inlogMessage = "";
-    $scope.checkLogin = function () {
+	$scope.ingelogd = false;
+	$scope.inlogMessage = "";
+	$scope.checkLogin = function () {
 
-    };
-    $scope.log = function () {
+	};
+	$scope.login = function (gegevens) {
+		dbService.login(gegevens, function(res) {
+			inlogMessage = res.message;
+		});
+	};
+	$scope.logout = function () {
+		dbService.logout(function(res) {
+			inlogMessage = res.message;
+		});
+	};
 
-        dbService.login.post($scope.login, function (data) {
-            console.log(data.message);
-
-        });
-    };
-    $scope.logout = function () {
-        dbService.logout(function (res) {
-            inlogMessage = res.message;
-        });
-    };
-
-    $scope.reg = function () {
-
-        if ($scope.createGebruiker.wachtwoord != $scope.createGebruiker.bevestigWachtwoord) {
-            $scope.IsMatch = true;
-            return false;
-        }
-        $scope.IsMatch = false;
-
-        dbService.createGebruiker.post($scope.createGebruiker, function (data) {
-            console.log(data.message);
-            console.log(data);
-        })
-    };
-};
+	$scope.registreer = function (gegevens) {
+		dbService.registreer(gegevens, function (res) {
+			inlogMessage = res.message;
+		});
+	}
+}
