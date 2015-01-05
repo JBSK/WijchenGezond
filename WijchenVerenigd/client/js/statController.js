012383fd8efd34f59bbdb00036c0549c275b19ce
Sanne Peters <sannepetersx@gmail.com>
2015-01-05 10:58:27 +0100

    Changed stator

--- WijchenVerenigd/client/js/statController.js
+++ WijchenVerenigd/client/js/statController.js
@@ -4,24 +4,25 @@ var statController = function ($routeParams, $scope, $window, dbService, statSer
 	var getTotaalStats = function() {
 		$scope.statistieken = [
 		{
-			icon : "fa fa-trophy",
-			pointer : "Punten totaal",
-			data : "574/750"
+
+			icon : "fa fa-users",
+			pointer : "Nu actief",
+			data : 5241
 		},
 		{
-			icon : "fa fa-line-chart",
+			icon : "fa fa-pie-chart",
 			pointer : "Punten vandaag",
 			data : 122
 		},
 		{
-			icon : "fa fa-pie-chart",
+			icon : "fa fa-line-chart",
 			pointer : "Populairste activiteit",
 			data : "Voetbal"
 		},
 		{
-			icon : "fa fa-users",
-			pointer : "Nu actief",
-			data : 5241
+			icon : "fa fa-trophy",
+			pointer : "Punten totaal",
+			data : "574/750"
 		}];
 	}
 
